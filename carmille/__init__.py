407c4ac0bcf73efd29a080e7d2b53247411509ed
Brendan O'Connor <brendan@maliceafterthought.com>
2021-01-10 12:21:28 -0800

    Moving the massive block of prewritten UI to a library so it's less ugly.

--- carmille/__init__.py
+++ carmille/__init__.py
@@ -5,7 +5,9 @@ Major chunks include:
   fetch: Responsible for interactions with the Slack API, like downloading messages and fetching
     user information.
   export: Responsible for taking the objects retrieved by fetch and excreting them into zip files.
+  ui: Holds prewritten Slack UI blocks to send as messages.
 """
 
 from . import fetch
 from . import export
+from . import ui
--- main.py
+++ main.py
@@ -6,112 +6,6 @@ from slack_bolt.async_app import AsyncApp
 
 import carmille as carmille
 
-ui_block = [
-    {
-    	'type': 'section',
-    	'text': {
-    		'type': 'mrkdwn',
-    		'text': 'Bonjour! I am happy to archive part of this channel\'s history for you.\n\n *Please select start and end times below (in your time zone):*'
-    	}
-    },
-    {
-    	'type': 'divider'
-    },
-    {
-    	'type': 'section',
-        'block_id': 'start-date',
-    	'text': {
-    		'type': 'mrkdwn',
-    		'text': 'Archive Start Date:'
-    	},
-    	'accessory': {
-    		'type': 'datepicker',
-    		'initial_date': '1990-04-28',
-    		'placeholder': {
-    			'type': 'plain_text',
-    			'text': 'Select a date',
-    			'emoji': True
-    		},
-    		'action_id': 'start-date'
-    	}
-    },
-    {
-    	'type': 'section',
-        'block_id': 'start-time',
-    	'text': {
-    		'type': 'mrkdwn',
-    		'text': 'Archive Start Time:'
-    	},
-    	'accessory': {
-    		'type': 'timepicker',
-    		'initial_time': '13:37',
-    		'placeholder': {
-    			'type': 'plain_text',
-    			'text': 'Select time'
-    		},
-    		'action_id': 'start-time'
-    	}
-    },
-    {
-    	'type': 'divider'
-    },
-    {
-    	'type': 'section',
-        'block_id': 'end-date',
-    	'text': {
-    		'type': 'mrkdwn',
-    		'text': 'Archive End Date:'
-    	},
-    	'accessory': {
-    		'type': 'datepicker',
-    		'initial_date': '1990-04-28',
-    		'placeholder': {
-    			'type': 'plain_text',
-    			'text': 'Select a date'
-    		},
-    		'action_id': 'end-date'
-    	}
-    },
-    {
-    	'type': 'section',
-        'block_id': 'end-time',
-    	'text': {
-    		'type': 'mrkdwn',
-    		'text': 'Archive End Time:'
-    	},
-    	'accessory': {
-    		'type': 'timepicker',
-    		'initial_time': '13:37',
-    		'placeholder': {
-    			'type': 'plain_text',
-    			'text': 'Select time'
-    		},
-    		'action_id': 'end-time'
-    	}
-    },
-    {
-    	'type': 'divider'
-    },
-    {
-    	'type': 'actions',
-    	'elements': [
-    		{
-    			'type': 'button',
-    			'text': {
-    				'type': 'plain_text',
-    				'text': 'Archive!'
-    			},
-    			'value': 'make_archive',
-                'action_id': 'make_archive'
-    		}
-    	]
-    },
-    {
-    	'type': 'divider'
-    }
-]
-
-
 app = AsyncApp()
 
 
@@ -124,6 +18,7 @@ async def command(ack, body, respond):
     initial_end_time = time.time() + user_tz_offset # Unix seconds
     initial_start_time = initial_end_time - 3600 # An hour earlier
     
+    ui_block = carmille.ui.datetime_selection_block
     
     # Start Times
     st = time.gmtime(initial_start_time)
